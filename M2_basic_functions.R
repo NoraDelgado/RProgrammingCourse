242492d599c1fcc4030b39bbdeff97b8c8b57fad
Nora Delgado <noradelgadobusot@gmail.com>
2025-01-24 16:09:45 -0500

    Add files via upload

---
+++ M2_basic_functions.R
@@ -0,0 +1,8 @@
+# Data
+assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 20, 20, 22)
+# Define function
+myMean <- function(assignment2){
+  return(sum(assignment2)/length(assignment2))
+}
+# Call function
+myMean(assignment2)
